8d5fe3f4ba156fe7e6496763ef825ae8d2ace3f4
gosuhiman <gosuhiman@gmail.com>
2018-12-01 23:40:35 +0100

    text change

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('yup yup');
+    expect(compiled.querySelector('h1').textContent).toContain('yup yup (づ｡◕‿‿◕｡)づ');
   });
 });
